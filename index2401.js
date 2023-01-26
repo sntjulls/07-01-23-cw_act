2421e226aa13144caca50f65a46c98ef5d400b6f
“Yulia <julie.kovalchuk@gmail.com>
2023-01-26 14:10:09 +0200

    Add example old syntax

--- index2401.js
+++ index2401.js
@@ -116,22 +116,22 @@
 
 //   console.log(vowel_count('Wb Development Tutorial')); //9
 
-// написати функцію, яка приймає рядок і повертає кількість голосних в рядку
-// регістр не враховувати (рахувати і великі і маленькі голосні)
-// працюємо тільки з латинецею
-// const vowels = ['a', 'e', 'i', 'u', 'o', 'y'];
-// не використовувати регулярні вирази!
+// // написати функцію, яка приймає рядок і повертає кількість голосних в рядку
+// // регістр не враховувати (рахувати і великі і маленькі голосні)
+// // працюємо тільки з латинецею
+// // const vowels = ['a', 'e', 'i', 'u', 'o', 'y'];
+// // не використовувати регулярні вирази!
 
-// спробувати оптимізувати функцію до одного ретурну (можна функцію-стрілку без ретурну взагалі)
+// // спробувати оптимізувати функцію до одного ретурну (можна функцію-стрілку без ретурну взагалі)
 
-const getVowels = (str, vowels = "aeiouy") => {
-  return str
-    .toLowerCase()
-    .split("")
-    .reduce((acc, cur) => (vowels.includes(cur) ? (acc += cur) : acc), "");
-};
+// const getVowels = (str, vowels = "aeiouy") => {
+//   return str
+//     .toLowerCase()
+//     .split("")
+//     .reduce((acc, cur) => (vowels.includes(cur) ? (acc += cur) : acc), "");
+// };
 
-const vowels = getVowels("They should expect stormier weather ahead");
+// const vowels = getVowels("They should expect stormier weather ahead");
 
-console.log(vowels.length);
+// console.log(vowels.length);
 
